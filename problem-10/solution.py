3a64c9bcd6d8a00fee9b0939a4a43559b33fc4ab
Henrique Leal <hm.leal@hotmail.com>
2017-11-03 16:32:15 +0000

    feat: Problem 10 solution

---
+++ problem-10/solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+def primes(limit):
+    """
+    Return a generator for prime numbers.
+
+    Args:
+      limit (int): The maximun prime number
+
+    This algorith uses the Sieve of Eratosthenes
+    https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    """
+    a = [True] * limit
+
+    a[0] = False
+    a[1] = False
+
+    for i, is_prime in enumerate(a):
+        if is_prime:
+            yield i
+
+            for n in range(i*i, limit, i):
+                a[n] = False
+
+
+if __name__ == '__main__':
+    print(sum([p for p in primes(2000000)]))
